f2e6c5fbe76ccb0ba4c30108d53d7d6133571ce0
Adam Tahir <adam.tahir@kellogg.northwestern.edu>
2025-02-25 02:50:19 +0000

    Routes created and places controller WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  resources "places"
+  get("/places", {:controller => "places", :action => "index"})
+  get("/places/:new", {:controller => "places", :action => "new"})
+  get("/places/:id", {:controller => "places", :action => "show"})
+  post("/places/create", {:controller => "places", :action => "create"})
+  
+  resources "entries"
+  get("entries", {:controller => "entries", :action => "index"})
+  get("/entries/:new", {:controller => "entries", :action =>"new"})
+  get("/entries/:id", {:controller => "entries", :action => "show"})
+  post("/entries/create", {:controller => "entries", :action => "create"})
+
 end
