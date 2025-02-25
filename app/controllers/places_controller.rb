b8543611473c8d6ec4810c22adf70fa211f90a84
Adam Tahir <adam.tahir@kellogg.northwestern.edu>
2025-02-25 03:08:37 +0000

    Fixed places controller and added new.html

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,8 +6,7 @@ end
 
 def show
   @place = Place.find_by({"id" => params["id"]})
-  @entries = Entry.where({"place_id" => params["id"]})
-  render("places/show")
+  @entries = Entry.where({"place_id" => @place["id"]})
 end
 
 def new
@@ -17,6 +16,23 @@ def create
   @place = Place.new
   @place["name"] = params["name"]
   @place.save
+  redirect_to "/places"
+end
+
+def edit 
+  @place = Place.find_by({"id" => params["id"]})
+end
+
+def update
+  @place = Place.find_by({"id" => params["id"]})
+  @place["name"] = params["name"]
+  @place.save
+  redirect_to "/places"
+end 
+
+def destroy
+  @place = Place.find_by({"id" => params["id"]})
+  @place.destroy
   redirect_to "/places"
 end
 
